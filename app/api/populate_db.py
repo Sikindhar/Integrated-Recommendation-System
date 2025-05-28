68230ec2ba2620cd0fd2f6e2dd9ee3cb4144ddca
Sikindhar <sikindharjaladi@gmail.com>
2025-05-29 03:17:57 +0530

    Add new collections for same Data

--- app/api/populate_db.py
+++ app/api/populate_db.py
@@ -7,17 +7,17 @@ load_dotenv()
 
 client = MongoClient(os.getenv('MONGODB_URI'))
 db = client[os.getenv('DATABASE_NAME')]
-users = db['users']
-products = db['products']
-ratings = db['ratings']
+new_users = db['new_users']
+new_products = db['new_products']
+new_ratings = db['new_ratings']
 
-ratings_df = pd.read_csv('data/processed_ratings.csv')
-products_df = pd.read_csv('data/processed_products.csv')
+ratings_df = pd.read_csv('app/data/processed_ratings.csv')
+products_df = pd.read_csv('app/data/processed_products.csv')
 
 ratings_records = ratings_df.to_dict('records')
 products_records = products_df.to_dict('records')
 
-ratings.insert_many(ratings_records)
-products.insert_many(products_records)
+new_ratings.insert_many(ratings_records)
+new_products.insert_many(products_records)
 
-ratings.create_index([('userId', 1), ('productId', 1)], unique=True)
+new_ratings.create_index([('userId', 1), ('productId', 1)], unique=True)
